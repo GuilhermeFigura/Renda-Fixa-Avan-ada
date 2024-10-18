0f9c83c6acb426b91a84da710458cafdeb78ae19
Guilherme <guilherme.figura@icloud.com>
2024-10-18 15:43:09 -0300

    Criação do enunciado

--- LFT.py
+++ LFT.py
@@ -1,7 +1,5 @@
 """Admita que uma instituição financeira tenha adquirido um lote de LTN em mercado primário. O prazo do título é de 108 dias corridos,
 correspondendo a 76 dias úteis. A instituição define uma taxa over anual de 11,8%. Pede-se:
-a. Fluxo de caixa da operação;
-b. PU equivalente (valor de face igual a R$ 1.000,00);
-c. Qual é a taxa de retorno do investimento (ao período)? """
-
+a. PU equivalente (valor de face igual a R$ 1.000,00);
+b. Qual é a taxa de retorno do investimento (ao período)? """
 
