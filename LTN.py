221bf1b4f6ac1e279cbf77104d4d9606ed0e22bf
Guilherme <guilherme.figura@icloud.com>
2024-10-18 16:10:01 -0300

    Resolução A

--- LTN.py
+++ LTN.py
@@ -3,3 +3,10 @@ correspondendo a 76 dias úteis. A instituição define uma taxa over anual de 1
 a. PU equivalente (valor de face igual a R$ 1.000,00);
 b. Qual é a taxa de retorno do investimento (ao período)? """
 
+ltn = 1000
+du = 76
+aao = 11.8 / 100
+
+PV = ltn / (1 + aao) ** (76/252)
+
+print(f'O P.U de compra por quantidade de LTN foi R$ {PV:.2f}.')
